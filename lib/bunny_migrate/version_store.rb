fa367660f7dd5715aa131a4f6266e6750c46be2b
Petris Fernandes <petrisrf@gmail.com>
2026-01-22 18:48:29 -0300

    Simple validation for VersionStore

--- lib/bunny_migrate/version_store.rb
+++ lib/bunny_migrate/version_store.rb
@@ -2,6 +2,8 @@ module BunnyMigrate
   class VersionStore < ::ActiveRecord::Base
     self.table_name = 'rabbitmq_migrations'
 
+    validates :version, :applied_at, presence: true
+
     before_save { self.applied_at = Time.now.utc }
   end
 end
