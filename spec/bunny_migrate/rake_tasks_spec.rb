f256ce6160e7f72149f9ff151a2d03617a1f1073
Petris Fernandes <petrisrf@gmail.com>
2026-02-08 03:01:06 -0300

    Test if install command exists

--- spec/bunny_migrate/rake_tasks_spec.rb
+++ spec/bunny_migrate/rake_tasks_spec.rb
@@ -5,6 +5,8 @@ require_relative '../../lib/bunny_migrate'
 RSpec.describe 'BunnyMigrate Rake tasks' do
   before(:all) { Rails.application.load_tasks }
 
+  it { expect(Rake::Task.task_defined?('bunny:install')).to be true }
+
   it { expect(Rake::Task.task_defined?('bunny:migrate')).to be true }
 
   it { expect(Rake::Task.task_defined?('bunny:rollback')).to be true }
